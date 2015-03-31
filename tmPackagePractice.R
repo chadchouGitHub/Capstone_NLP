ae016b0b1e51d93c10e6d717ffd54ca9606bbd6d
chadchouGitHub <chiachingchou@gmail.com>
2015-03-31 09:29:49 -0400

    NLP tm package
    practice

---
+++ tmPackagePractice.R
@@ -0,0 +1,8 @@
+## Practice NLP with tm package and other NLP package
+
+library("NLP") #Generics NLP Function set
+library("openNLP") #Generics NLP Function set
+library("tm") #For Text Mining & Corpus workings
+library("RWeka") #For n-gram vector generation
+library("qdap") #For Text Mining & Corpus workings
+library("ggplot2") #Charting functionality
