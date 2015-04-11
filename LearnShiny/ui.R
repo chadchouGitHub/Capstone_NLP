2039549ea22e94c5cb3dd519cb77180ea12db610
chadchouGitHub <chiachingchou@gmail.com>
2015-04-11 09:52:39 -0400

    Lesson3
    Still has many place not show up correct with layout.

--- LearnShiny/ui.R
+++ LearnShiny/ui.R
@@ -1,53 +1,81 @@
-library(shiny)
+
 
 # Define UI for application that draws a histogram
 ## Lesson 2 by make new ui.R and server.R 
 ## position="right" in sidebarLayout, and html <h1> <h2>.... to h1(), h2()... in R
 ## also many html text format. See after p().at line 38..
+## Lesson 3 Adding widgets
 shinyUI(fluidPage(
+        titlePanel("Basic widgets"),
         
-        # Application title
-        titlePanel("Hello world!"),
+        fluidRow(
+                
+                column(3,
+                       h3("Buttons"),
+                       actionButton("action", label = "Action"),
+                       br(),
+                       br(), 
+                       submitButton("Submit")),
+                
+                column(3,
+                       h3("Single checkbox"),
+                       checkboxInput("checkbox", label = "Choice A", value = TRUE)),
+                
+                column(3, 
+                       checkboxGroupInput("checkGroup", 
+                                          label = h3("Checkbox group"), 
+                                          choices = list("Choice 1" = 1, 
+                                                         "Choice 2" = 2, "Choice 3" = 3),
+                                          selected = 1)),
+                
+                column(3, 
+                       dateInput("date", 
+                                 label = h3("Date input"), 
+                                 value = "2014-01-01"))   
+        ),
         
-        # Sidebar with a slider input for the number of bins
-        sidebarLayout(position = "right",
-                sidebarPanel(
-                        h1("What is going to happen"),
-                        img(src = "fig1.png", height = 38, width = 71),
-                        sliderInput("bins",
-                                    "Number of bins:",
-                                    min = 5,
-                                    max = 50,
-                                    value = 30)
+        fluidRow(
+                
+                column(3,
+                       dateRangeInput("dates", label = h3("Date range"))),
+                
+                column(3,
+                       fileInput("file", label = h3("File input"))),
+                
+                column(3, 
+                       h3("Help text"),
+                       helpText("Note: help text isn't a true widget,", 
+                                "but it provides an easy way to add text to",
+                                "accompany other widgets.")),
+                
+                column(3, 
+                       numericInput("num", 
+                                    label = h3("Numeric input"), 
+                                    value = 1))   
+        ),
+        
+        fluidRow(
+                
+                column(3,
+                       radioButtons("radio", label = h3("Radio buttons"),
+                                    choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                                   "Choice 3" = 3),selected = 1)),
+                
+                column(3,
+                       selectInput("select", label = h3("Select box"), 
+                                   choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                                  "Choice 3" = 3), selected = 1)),
+                
+                column(3, 
+                       sliderInput("slider1", label = h3("Sliders"),
+                                   min = 0, max = 100, value = 50),
+                       sliderInput("slider2", "",
+                                   min = 0, max = 100, value = c(25, 75))
                 ),
                 
-                # Show a plot of the generated distribution
-                mainPanel(
-                        plotOutput("distPlot"),
-                        h1("First level title"),
-                        h2("Second level title"),
-                        h3("Third level title"),
-                        h4("Fourth level title"),
-                        h5("Fifth level title"),
-                        h6("Sixth level title"),
-                        h6("use align= center in h6()", align = "center"), ## use align="center" in h6()
-                        h6("A NEW HOPE", align = "center"),
-                        h5("It is a period of civil war.", align = "center"),
-                        h4("Rebel spaceships, striking", align = "center"),
-                        h3("from a hidden base, have won", align = "center"),
-                        h2("their first victory against the", align = "center"),
-                        p("p creates a paragraph of text."),
-                        p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
-                        strong("strong() makes bold text."),
-                        em("em() creates italicized (i.e, emphasized) text."),
-                        br(),
-                        code("code displays your text similar to computer code"),
-                        div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
-                        br(),
-                        p("span does the same thing as div, but it works with",
-                          span("groups of words", style = "color:blue"),
-                          "that appear inside a paragraph."),
-                        img(src = "fig1.png", height = 381, width = 712) ## fig must in www folder
-                )
+                column(3, 
+                       textInput("text", label = h3("Text input"), 
+                                 value = "Enter text..."))   
         )
+        
 ))
