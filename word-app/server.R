a2b729983a027461c7533a40fa04ee2cbaf9c4f8
chadchouGitHub <chiachingchou@gmail.com>
2015-04-20 11:30:17 -0400

    The first layout
    The first layout of my shiny app

--- word-app/server.R
+++ word-app/server.R
@@ -1,45 +1,17 @@
-# server.R for word-app
-
-source("~/word-app/wordHelper.R") # This will load percent_map function
-## I find I need to put ~/ as home directory to have file load for shiny.
-## I don't need ~/ for home directory when I run the commond in console
-load("~/word-app/data/sortedOneTwoTri.RData") ## loading data
-## percent_map(counties$white, "darkgreen", "% white") ## make map... but the ledgen are too big to covery the map in preview.
 
 
-# b <- c("is some thing I need to show you")
+
+
+
+
 ##--------------------code above here will run only once in shiny-----------------------
 shinyServer(
         function(input, output) {
                 ## -----another place for code-----
                 
                 ## ----- another place for code-----
-                output$map <- renderPlot({
-                        
-                        data <- switch(input$var, 
-                                       "Percent White" = counties$white,
-                                       "Percent Black" = counties$black,
-                                       "Percent Hispanic" = counties$hispanic,
-                                       "Percent Asian" = counties$asian)
-                        
-                        color <- switch(input$var, 
-                                        "Percent White" = "darkgreen",
-                                        "Percent Black" = "black",
-                                        "Percent Hispanic" = "darkorange",
-                                        "Percent Asian" = "darkviolet")
-                        
-                        legend <- switch(input$var, 
-                                         "Percent White" = "% White",
-                                         "Percent Black" = "% Black",
-                                         "Percent Hispanic" = "% Hispanic",
-                                         "Percent Asian" = "% Asian")
-                        
-                        percent_map(var = data, 
-                                    color = color, 
-                                    legend.title = legend, 
-                                    max = input$range[2], 
-                                    min = input$range[1])
-                })
+                output$text1 <- renderText({ 
+                        paste("You have selected", input$inText)})
                 ## -----third place for code-----
                 
                 ## -----third place for code-----
--- word-app/ui.R
+++ word-app/ui.R
@@ -1,24 +1,14 @@
-# ui.R for word-app
 shinyUI(fluidPage(
-        titlePanel("Predicting Next Word"),
+        titlePanel("Shiny App: Predicting your next word."),
         
         sidebarLayout(
                 sidebarPanel(
-                        helpText("Create demographic maps with 
-                                 information from the 2010 US Census."),
+                        helpText("Please type words in the text input box."),
                         
-                        selectInput("var", 
-                                    label = "Choose a variable to display",
-                                    choices = c("Percent White", "Percent Black",
-                                                "Percent Hispanic", "Percent Asian"),
-                                    selected = "Percent White"),
-                        
-                        sliderInput("range", 
-                                    label = "Range of interest:",
-                                    min = 0, max = 100, value = c(0, 100))
-                ),
+                        textInput("inText", label = h3("Text input"), 
+                                  value = "Enter text...")),
                 
-                mainPanel(plotOutput("map"))
+                mainPanel(textOutput("text1"))
         )
         
 ))
